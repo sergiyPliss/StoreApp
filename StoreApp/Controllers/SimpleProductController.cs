38c4e5f28f0a756be5ae95fb83513d6967bbe429
Sergiy <plisssergiy@gmail.com>
2024-04-16 12:25:38 -0400

    Changed values on products.

--- StoreApp/Controllers/SimpleProductController.cs
+++ StoreApp/Controllers/SimpleProductController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Shmoceries", Price = 2 },
+            new Product { Id = 2, Name = "Kendama", Category = "Memorabilia", Price = 4.75M },
+            new Product { Id = 3, Name = "Crowbar", Category = "Half Life Merchandise", Price = 17.99M }
         };
         public ProductsController()
         {
